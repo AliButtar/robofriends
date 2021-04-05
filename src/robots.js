121d79d15cdcb8e6556c534bec9d1c5c0edf865e
Muhammad Ali Buttar <aliasifbuttar@icloud.com>
2021-04-06 01:10:07 +0500

    New Cards

--- src/robots.js
+++ src/robots.js
@@ -19,9 +19,9 @@ export const robots = [
     },
     {
       id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org'
+      name: 'Talha Bin Mansoor',
+      username: 'Elvin Arrow',
+      email: 'tbm@tbm.com'
     },
     {
       id: 5,
